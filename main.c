543c26bd47c76e554f9eccff0b8f6df92d9a4bc4
ChenTianyi-0311 <80047128+ChenTianyi-0311@users.noreply.github.com>
2021-04-21 19:27:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,19 +2,19 @@
 
 void main( void )
 {
-    WDTCTL = WDTPW | WDTHOLD;   // arret du Watchdog
+    WDTCTL = WDTPW | WDTHOLD;           // arret du Watchdog
     P1DIR |= BIT6;
     P1DIR &= ~(BIT3);
-    P1REN |= BIT3;              // activation resistance interne
-    P1OUT |= BIT3;              // mode pull-up
-    P1IE |= BIT3;               // activation interruption
-    P1IES |= BIT3;              // interruption sur front descendant
-                                // donc appui car bouton connecte a la masse
-    P1IFG &= ~(BIT3);           // RAZ flag d’interruption
-    __enable_interrupt();         // autorisation generale des interruptions
+    P1REN |= BIT3;                      // activation resistance interne
+    P1OUT |= BIT3;                      // mode pull-up
+    P1IE |= BIT3;                       // activation interruption
+    P1IES |= BIT3;                      // interruption sur front descendant
+                                        // donc appui car bouton connecte a la masse
+    P1IFG &= ~(BIT3);                   // RAZ flag d’interruption
+    __enable_interrupt();               // autorisation generale des interruptions
     while(1)
     {
-        __delay_cycles(500000);       //delai pour simuler un traitement
+        __delay_cycles(500000);         //delai pour simuler un traitement
     }
 }
 
