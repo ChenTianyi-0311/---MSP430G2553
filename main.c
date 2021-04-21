6e4775af3e424fc23fcb10effb9a6369d970bfb1
ChenTianyi-0311 <80047128+ChenTianyi-0311@users.noreply.github.com>
2021-04-21 19:26:02 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,27 @@
-#include <msp430g2553.h>
+#include <msp430.h> 
+
 void main( void )
 {
-  WDTCTL = WDTPW | WDTHOLD; // arret du Watchdog
-  P1DIR |= BIT6;
-  P1DIR &= ~(BIT3);
-  P1REN |= BIT3;  // activation resistance interne
-  P1OUT |= BIT3;  // mode pull-up
-  P1IE |= BIT3;   // activation interruption
-  P1IES |= BIT3;  // interruption sur front descendant
-                  // donc appui car bouton connecte a la masse
-  P1IFG &= ~(BIT3); // RAZ flag d’interruption
-  __enable_interrupt(); // autorisation generale des interruptions
-  while(1)
-  {
-  __delay_cycles(500000); //delai pour simuler un traitement
-  }
+    WDTCTL = WDTPW | WDTHOLD;   // arret du Watchdog
+    P1DIR |= BIT6;
+    P1DIR &= ~(BIT3);
+    P1REN |= BIT3;              // activation resistance interne
+    P1OUT |= BIT3;              // mode pull-up
+    P1IE |= BIT3;               // activation interruption
+    P1IES |= BIT3;              // interruption sur front descendant
+                                // donc appui car bouton connecte a la masse
+    P1IFG &= ~(BIT3);           // RAZ flag d’interruption
+    __enable_interrupt();         // autorisation generale des interruptions
+    while(1)
+    {
+        __delay_cycles(500000);       //delai pour simuler un traitement
+    }
 }
 
 #pragma vector=PORT1_VECTOR
 __interrupt void detect_bouton(void)
 {
-  P1OUT ^= (BIT6) ;
-  __delay_cycles(100000);
-  P1IFG &= ~(BIT3);
-} 
+    P1OUT ^= (BIT6) ;
+    __delay_cycles(1000000);
+    P1IFG &= ~(BIT3);
+}
